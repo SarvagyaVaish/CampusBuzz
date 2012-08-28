764eefd6cf65b47f9ff72b31adc8f11714058c4c
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-08-28 16:28:51 -0400

    configured carrierwave for image storage using amazon s3

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :end_dt, :image, :key, :keytype, :link, :name, :published, :start_dt, :venue_id
+
+  mount_uploader :image, ImageUploader
 end
