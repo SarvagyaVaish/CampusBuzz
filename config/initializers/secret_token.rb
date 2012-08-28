b4c5e5456f8ea7603ff59ce02cff4c17880e2eac
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-08-28 14:05:15 -0400

    Initial commit. Added foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusBuzz::Application.config.secret_token = '2b8d0ba022e7ccef418ceefb73de98e4ef4b5bcbd436cc337371b499e87c121f5f88d7f5455febd705696f5e993b5964bebee8c52daf4b1be5623750ac54fceb'
